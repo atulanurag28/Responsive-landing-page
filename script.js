92f7e8746e07afc4657fddb4d30a763baba708f5
Atul Anand <97800127+atulanurag28@users.noreply.github.com>
2024-05-21 11:27:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+var animation=bodymovin.loadAnimation({
+    container:document.getElementById('animContainer'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'https://assets5.lottiefiles.com/packages/lf20_r8nda3zk.json'
+
+})
+
+var animation1=bodymovin.loadAnimation({
+    container:document.getElementById('animContainer1'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'https://assets8.lottiefiles.com/packages/lf20_tFYxgv.json'
+
+})
+
+var animation2=bodymovin.loadAnimation({
+    container:document.getElementById('animContainer2'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'https://assets5.lottiefiles.com/packages/lf20_mxrkk5de.json'
+
+})
+
+var animation3=bodymovin.loadAnimation({
+    container:document.getElementById('animContainer3'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'https://assets1.lottiefiles.com/packages/lf20_hssvolmu.json'
+
+})
